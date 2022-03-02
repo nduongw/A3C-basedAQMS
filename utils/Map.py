9c913717ed97a995c5227272598eec2b7405ac23
Manh Cuong <daomanhcuong2001@gmail.com>
2022-03-02 10:56:56 +0700

    add num_frame in config

--- utils/Map.py
+++ utils/Map.py
@@ -1,20 +1,20 @@
-import random
-import numpy as np
-
 from utils.Utils import *
-from utils.Reward import calc_reward
+import random
 
 class Map:
     def __init__(self, Config):
         self.Config = Config
-        self.map = torch.zeros(Config.get('road_length'), Config.get('road_width'))
-        self.cover_map = torch.zeros(Config.get('road_length'), Config.get('road_width'))
+        self.map = torch.zeros(Config.get('roadLength'), Config.get('roadWidth'))
+        self.cover_map = []
+        self.time = 0
     
     def create_map(self):
         self.map = generate_map(self.Config)
-        self.cover_map = generate_air_quality_map(self.map, self.Config)
-        # return np.stack(self.map, self.cover_map)
-        return self.map, self.cover_map
+        return self.map
+
+    def set_cover_radius(self):
+        cover_map = generate_air_quality_map(self.map, self.Config)
+        return cover_map
 
     def run_per_second(self):
         # cho xe di chuyen
@@ -38,32 +38,26 @@ class Map:
                     else:
                         self.map[i, j] = 1
 
-        #tinh lai do bao phu
-        previous_map = self.cover_map
+        previous_map = self.cover_map[-1]
         previous_map -= self.Config.get('air_discount') * previous_map
         zeros_tensor = torch.zeros(previous_map.size())
         previous_map = torch.where(previous_map > 0, previous_map, zeros_tensor)
-        self.cover_map = previous_map
-        return self.map, self.cover_map
+        self.cover_map.append(previous_map)
+        self.time += 1
     
     def step(self, prob_map):
-        action = self.map_to_action(prob_map)
-        new_map = torch.zeros(self.Config.get('roadLength'), self.Config.get('roadWidth'))
-        cover_map = set_cover_radius(new_map, self.Config.get('cover_radius'), action)
-        new_cover_map = torch.where(self.map > cover_map, self.map, cover_map)
-        reward = calc_reward(action, self.cover_map, self.map, new_cover_map)
-        self.cover_map = new_cover_map
-
-        return self.map, self.cover_map, reward
-    
-    def map_to_action(self, prob_map):
         on_off_map = torch.where(prob_map > self.Config.get('action_prob'), 1, 0)
         num_car = count_car(on_off_map)
         action_map = set_cover_radius(on_off_map, self.Config.get('action_range'), num_car)
         action = torch.where(self.init_map == action_map, self.init_map, 0)
         return action
+    
+    def recalc_cover_map(self, prob_map):
+        action = self.step(prob_map)
+        new_map = torch.zeros(self.Config.get('roadLength'), self.Config.get('roadWidth'))
+        cover_map = set_cover_radius(new_map, self.Config.get('cover_radius'), action)
+        new_cover_map = torch.where(self.map > cover_map, self.map, cover_map)
+        self.cover_map.append(new_cover_map)
 
-# demo = Map(Config)
-# a1, a2 = demo.create_map()
-# print(a1)
-# print(a2)
+        return list(new_cover_map, self.map)
+
